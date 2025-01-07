b1fb9358b60c57d0b0dbe868cb3997b518bb9a03
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2025-01-07 22:29:48 +0200

    fw: use SDK directly for button and led

--- firmware/src/components/blinker.hpp
+++ firmware/src/components/blinker.hpp
@@ -1,20 +1,48 @@
 #pragma once
-
-#include <Arduino.h>
+#include "driver/ledc.h"
+#include "driver/periph_ctrl.h"
+#include "esp_timer.h"
 #include "lib/blinker_engine.hpp"
 
 class LedDriver : public IBlinkerLED<1> {
 public:
-    void set(const DataType& value) override {
-        if (!initialized) {
-            initialized = true;
-            pinMode(ledPin, OUTPUT);
-        }
-        analogWrite(ledPin, 255 - value[0]);  // Write inverted PWM to GP8
+    LedDriver() {
+        periph_module_enable(PERIPH_LEDC_MODULE);
+
+        ledc_timer_config_t timer_conf = {
+            .speed_mode = LEDC_LOW_SPEED_MODE,
+            .duty_resolution = LEDC_TIMER_8_BIT,
+            .timer_num = timerNum,
+            .freq_hz = 5000,
+            .clk_cfg = LEDC_AUTO_CLK
+        };
+        ledc_timer_config(&timer_conf);
+
+        ledc_channel_config_t ch_conf = {
+            .gpio_num = ledPin,
+            .speed_mode = LEDC_LOW_SPEED_MODE,
+            .channel = channelNum,
+            .intr_type = LEDC_INTR_DISABLE,
+            .timer_sel = timerNum,
+            .duty = 0,
+            .hpoint = 0,
+            .flags = {
+                // Output should be inverted, because of the way the LED is connected
+                .output_invert = 1
+            }
+        };
+        ledc_channel_config(&ch_conf);
     }
+
+    void set(const DataType& value) override {
+        ledc_set_duty(LEDC_LOW_SPEED_MODE, channelNum, value[0]);
+        ledc_update_duty(LEDC_LOW_SPEED_MODE, channelNum);
+    }
+
 private:
-    static constexpr uint8_t ledPin{8};
-    bool initialized{false};
+    static constexpr int ledPin{GPIO_NUM_8};
+    static constexpr ledc_timer_t timerNum{LEDC_TIMER_0};
+    static constexpr ledc_channel_t channelNum{LEDC_CHANNEL_0};
 };
 
 template <typename Driver>
@@ -24,7 +52,7 @@ private:
 
     static void timerCallback(TimerHandle_t timer) {
         Blinker* self = static_cast<Blinker*>(pvTimerGetTimerID(timer));
-        self->tick(millis());
+        self->tick(esp_timer_get_time() / 1000);
     }
 
 public:
--- firmware/src/components/button.hpp
+++ firmware/src/components/button.hpp
@@ -1,20 +1,28 @@
 #pragma once
 
-#include <Arduino.h>
+#include "driver/gpio.h"
+#include "esp_timer.h"
 #include "lib/button_engine.hpp"
 
 class ButtonDriver : public IButtonDriver {
 public:
     auto get() -> bool override {
-        if (!initialized) {
-            initialized = true;
-            pinMode(btnPin, INPUT_PULLUP);
+        static bool init_done = false;
+        if (!init_done) {
+            gpio_config_t io_conf = {
+                .pin_bit_mask = (1ULL << btnPin),
+                .mode = GPIO_MODE_INPUT,
+                .pull_up_en = GPIO_PULLUP_ENABLE,
+                .pull_down_en = GPIO_PULLDOWN_DISABLE,
+                .intr_type = GPIO_INTR_DISABLE
+            };
+            gpio_config(&io_conf);
+            init_done = true;
         }
-        return digitalRead(btnPin) == LOW;
+        return gpio_get_level(btnPin) == 0;
     }
 private:
-    static constexpr uint8_t btnPin{9};
-    bool initialized{false};
+    static constexpr gpio_num_t btnPin{GPIO_NUM_9};
 };
 
 template <typename Driver>
@@ -25,7 +33,7 @@ public:
             [](void* params) {
                 auto* self = static_cast<Button*>(params);
                 while (true) {
-                    self->tick(millis());
+                    self->tick(esp_timer_get_time() / 1000);
                     vTaskDelay(pdMS_TO_TICKS(10));
                 }
             }, "button", 1024*4, this, 4, nullptr
