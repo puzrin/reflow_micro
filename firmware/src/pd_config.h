3e9402963266a075fa85b9e5551ec283d7779d85
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2025-09-13 16:26:37 +0300

    fw: update pdsink and error log level

--- firmware/src/pd_config.h
+++ firmware/src/pd_config.h
@@ -8,13 +8,12 @@
 #define PD_LOG_FN_LVL_DEBUG     jetlog::level::debug
 #define PD_LOG_FN_LVL_VERBOSE   jetlog::level::verbose
 
-// Use for debugging
-// #define PD_LOG_DEFAULT DEBUG
+#define PD_LOG_DEFAULT ERR
 
-// #define PD_LOG_PE INFO
-// #define PD_LOG_PRL INFO
 #define PD_LOG_TC INFO
 #define PD_LOG_DPM INFO
+// #define PD_LOG_PE INFO
+// #define PD_LOG_PRL INFO
 
 
 #define USE_FUSB302_RTOS
