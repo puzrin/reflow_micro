82ed05e508f23c8ec116e8f6b36203159283847a
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2025-09-15 01:02:34 +0300

    fw: pdsink update

--- firmware/src/pd_config.h
+++ firmware/src/pd_config.h
@@ -3,12 +3,12 @@
 #include "logger.hpp"
 
 #define PD_LOG_FN(...)          logger.push(__VA_ARGS__)
-#define PD_LOG_FN_LVL_ERR       jetlog::level::error
+#define PD_LOG_FN_LVL_ERROR     jetlog::level::error
 #define PD_LOG_FN_LVL_INFO      jetlog::level::info
 #define PD_LOG_FN_LVL_DEBUG     jetlog::level::debug
 #define PD_LOG_FN_LVL_VERBOSE   jetlog::level::verbose
 
-#define PD_LOG_DEFAULT ERR
+#define PD_LOG_DEFAULT ERROR
 
 #define PD_LOG_TC INFO
 #define PD_LOG_DPM INFO
