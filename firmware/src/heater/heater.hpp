4abc2fdd9f70d6f9f9652f346caa0c401798ca76
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2025-12-21 11:37:52 +0200

    fw: remove HeaterControlMock

--- firmware/src/heater/heater.hpp
+++ firmware/src/heater/heater.hpp
@@ -1,11 +1,6 @@
 #pragma once
 
-#if defined(RFL_USE_MOCKS)
-#include "heater_control_mock.hpp"
-using Heater = HeaterControlMock;
-#else
 #include "heater_control.hpp"
 using Heater = HeaterControl;
-#endif
 
 inline Heater heater;
