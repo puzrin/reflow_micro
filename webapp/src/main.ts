28d7cb9c8d432802b2b09e63b81e384d3b9aab5c
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2026-03-22 04:27:50 +0200

    web: reduca loaded fonts

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -1,9 +1,8 @@
 import './main.css'
 import 'virtual:uno.css'
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
+import '@fontsource/roboto/latin-400.css'
+import '@fontsource/roboto/latin-500.css'
+import '@fontsource/roboto/latin-700.css'
 
 import { createApp, watch } from 'vue'
 import { createPinia } from 'pinia'
